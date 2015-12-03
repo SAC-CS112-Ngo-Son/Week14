ce24a99a636257fbb1c1916e2586cc11e9ecb7fc
SAC-CS112-Ngo-Son <frank.ngo.1702@gmail.com>
2015-12-02 21:13:38 -0800

    lab 2

--- src/Low_High.java
+++ src/Low_High.java
@@ -1,29 +1,44 @@
+
 //Son Ngo
 
 import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class Low_High
 {
 	private int iDice, iThrow;
-	
-	//default constructor
+
+	// default constructor
 	public Low_High()
 	{
 		iDice = 1;
 	}
-	
-	//constructor with 1 variable carrying the number of dices
-	public Low_High (int dices)
+
+	// constructor with 1 variable carrying the number of dices
+	public Low_High(int dices)
 	{
-		iDice = dices;
+		if (!this.isLessThanZero(dices))
+			iDice = dices;
+		else
+			throw new IllegalArgumentException();
 	}
-	
-	//method to return the result of the throw (low or high)
+
+	public boolean isLessThanZero(int aDiceNumber)
+	{
+		if (aDiceNumber <= 0)
+			return true;
+		else
+			return false;
+	}
+
+	// method to return the result of the throw (low or high)
 	public char Low_Or_High()
 	{
 		Random rand = new Random();
-		
+
 		iThrow = (rand.nextInt(6) + 1) * iDice;
-		
+
 		if (iThrow <= (iDice * 3))
 			return 'L';
 		else
